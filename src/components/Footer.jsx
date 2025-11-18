5d4926de864d9585294c997e4fc9fa0c4bb87ed0
DevSagar007 <sagorkhan9294@gmail.com>
2025-11-19 00:04:41 +0600

    added some component like hero footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer class="py-6 md:py-8">
+        <div class="container mx-auto">
+          <p class="text-center text-xs text-[#6A6A6A] lg:text-sm">
+            Copyright ©2024 | All rights reserved by Learn with Sumit
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
