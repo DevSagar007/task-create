48077cdb5f8967bfc251e89b709df3c78549f082
DevSagar007 <sagorkhan9294@gmail.com>
2025-11-25 01:33:25 +0600

    working a new task

--- src/components/task/TaskBoard.jsx
+++ src/components/task/TaskBoard.jsx
@@ -3,8 +3,10 @@ import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 function TaskBoard() {
+
   const defaultTask = {
     id: crypto.randomUUID(),
     title: "API Data Synchronization with java",
@@ -12,19 +14,30 @@ function TaskBoard() {
       "Implement a Python solution to synchronize data between an API and a third-party database securely, optimizing data exchange.",
     tags: ["Web", "Python", "API"],
     priority: "High",
-    isCompleted: false,
+    isFavourite: true,
   };
+
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddTaskModal, setShowAddTaskModal] = useState(false);
+
+  function handleAddTask(task) {
+    console.log('Adding a task', task);
+    
+  }
 
   return (
     <>
       <section className="mb-20" id="tasks">
+        {/* Modal show */}
+        {showAddTaskModal && <AddTaskModal onSave={handleAddTask} />}
+
         <div className="container mx-auto">
           <div className="p-2 flex justify-end">
             <SearchTask />
           </div>
+
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <TaskActions />
+            <TaskActions onAddTask={() => setShowAddTaskModal(true)} />
             <TaskList tasks={tasks} />
           </div>
         </div>
--- src/components/task/TaskList.jsx
+++ src/components/task/TaskList.jsx
@@ -1,3 +1,5 @@
+import { FaStar } from "react-icons/fa";
+
 function TaskList({ tasks }) {
   return (
     <>
@@ -35,21 +37,11 @@ function TaskList({ tasks }) {
                 className="border-b border-[#2E3443] [&>td]:align-baseline [&>td]:px-4 [&>td]:py-2"
               >
                 <td>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="icon icon-tabler icon-tabler-star"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    strokeWidth="2"
-                    stroke="yellow"
-                    fill="yellow"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
-                  </svg>
+                  {tasks.isFavourite ? (
+                    <FaStar color="yellow" />
+                  ) : (
+                    <FaStar color="gray" />
+                  )}
                 </td>
                 <td>{tasks.title}</td>
                 <td>
@@ -69,7 +61,7 @@ function TaskList({ tasks }) {
                 <td className="text-center">{tasks.priority}</td>
                 <td>
                   <div className="flex items-center justify-center space-x-3">
-                    <button className="text-red-500">Delete</button>
+                    <button type="button" className="text-red-500">Delete</button>
                     <button className="text-blue-500">Edit</button>
                   </div>
                 </td>
